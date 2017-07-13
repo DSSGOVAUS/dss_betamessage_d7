9b4c8fcf9b276a37e8a93dec3b5e9dc9ed3bf2b2
Tally Tarik <tally.tarik@gmail.com>
2017-07-13 16:28:19 +1000

    Update styling, add config option to change where "beta" tag is attached

--- dss_betamessage/betamessage.js
+++ dss_betamessage/betamessage.js
@@ -1,17 +1,23 @@
-jQuery(function($) {$(document).ready(function(){     
-	$('#wrapper').append('<a href="#" class="betamessage-tag betamessage-open">Beta</a>');
-	if((typeof(Storage)!=="undefined") && (localStorage.getItem('betamessage') != 'closed')) {
-		// Show the message
-		$('.betamessage').css('display','block');
-	}            
-	// Clicks the Beta link
-	$('.betamessage-open').click(function(){
-		localStorage.setItem('betamessage', '');
-		$('.betamessage').slideDown(500);
-	});   
-	// Clicks the Close
-	$('.betamessage-close').click(function(){
-		$('.betamessage').slideUp(500);
-		localStorage.setItem('betamessage', 'closed');
+jQuery(function($) {
+	$(document).ready(function() {     
+		var wrapper_element = $('.betamessage').data("attach-to");
+
+		$(wrapper_element).css("position", "relative");
+		$(wrapper_element).append('<a href="#" class="betamessage-tag betamessage-open">Beta</a>');
+
+		if((typeof(Storage)!=="undefined") && (localStorage.getItem('betamessage') != 'closed')) {
+			// Show the message
+			$('.betamessage').css('display','block');
+		}            
+		// Clicks the Beta link
+		$('.betamessage-open').click(function(){
+			localStorage.setItem('betamessage', '');
+			$('.betamessage').slideDown(500);
+		});   
+		// Clicks the Close
+		$('.betamessage-close').click(function(){
+			$('.betamessage').slideUp(500);
+			localStorage.setItem('betamessage', 'closed');
+		});
 	});
-});});
+});
