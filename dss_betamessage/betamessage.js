c8254d415bbe88531ab7ae5f2b20bc7b9b862155
Mel Stubbings <>
2022-03-18 10:09:29 +1100

    Add check for beta-hide class

--- dss_betamessage/betamessage.js
+++ dss_betamessage/betamessage.js
@@ -1,32 +1,39 @@
-jQuery(function($) {
-	$(document).ready(function() {
-		var wrapper_element = $('.betamessage').data('attach-to');
+jQuery(function ($) {
+  $(document).ready(function () {
+    var wrapper_element = $(".betamessage").data("attach-to");
 
-		$(wrapper_element).css('position', 'relative');
-		$(wrapper_element).append('<a href="#" class="betamessage-tag betamessage-open" aria-label="Display beta message">Beta</a>');
-
-		if((typeof(Storage)!=='undefined') && (localStorage.getItem('betamessage') != 'closed')) {
-			// Show the message
-			if($('body').hasClass('front')) {
-			  $('.betamessage').slideDown(500);
-				$('body').addClass('is-beta-shown');
-			}
-			else {
-				$('.betamessage').css('display','block');
-				$('body').addClass('is-beta-shown');
-			}
-		}
-		// Clicks the Beta link
-		$('.betamessage-open').click(function(){
-			localStorage.setItem('betamessage', '');
-			$('.betamessage').slideDown(500);
-			$('body').addClass('is-beta-shown');$('body').removeClass('is-beta-hidden');
-		});
-		// Clicks the Close
-		$('.betamessage-close').click(function(){
-			$('.betamessage').slideUp(500);
-			localStorage.setItem('betamessage', 'closed');
-			$('body').addClass('is-beta-hidden');$('body').removeClass('is-beta-shown');
-		});
-	});
+    $(wrapper_element).css("position", "relative");
+    if (!$(".betamessage").hasClass("hide-beta")) {
+      $(wrapper_element).append(
+        '<a href="#" class="betamessage-tag betamessage-open" aria-label="Display beta message">Beta</a>'
+      );
+    }
+    if (
+      typeof Storage !== "undefined" &&
+      localStorage.getItem("betamessage") != "closed"
+    ) {
+      // Show the message
+      if ($("body").hasClass("front")) {
+        $(".betamessage").slideDown(500);
+        $("body").addClass("is-beta-shown");
+      } else {
+        $(".betamessage").css("display", "block");
+        $("body").addClass("is-beta-shown");
+      }
+    }
+    // Clicks the Beta link
+    $(".betamessage-open").click(function () {
+      localStorage.setItem("betamessage", "");
+      $(".betamessage").slideDown(500);
+      $("body").addClass("is-beta-shown");
+      $("body").removeClass("is-beta-hidden");
+    });
+    // Clicks the Close
+    $(".betamessage-close").click(function () {
+      $(".betamessage").slideUp(500);
+      localStorage.setItem("betamessage", "closed");
+      $("body").addClass("is-beta-hidden");
+      $("body").removeClass("is-beta-shown");
+    });
+  });
 });
